e1fc17bccb92555de2303e83de0ed70381b8f6b9
Adrian Creegan <adrian.creegan2@bd.com>
2023-02-04 14:44:25 +0000

    Missed this one.

--- SLDNet_code/train_video_rain.sh
+++ SLDNet_code/train_video_rain.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2 python train_video_rain.py -checkpoint_dir ./checkpoints/P202_video_rain_pretrain_formal_code/ -data_dir Video_rain -list_filename ./lists/video_rain_removal_train.txt -crop_size 64
+python train_video_rain.py -checkpoint_dir ./checkpoints/P202_video_rain_pretrain_formal_code/ -data_dir Video_rain -list_filename ./lists/video_rain_removal_train.txt -crop_size 64
