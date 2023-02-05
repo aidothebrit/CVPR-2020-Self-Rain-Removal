f44f479c2ba8c2008f887119959e6b720eb9cc7c
Adrian Creegan <adrian.creegan2@bd.com>
2023-02-05 19:38:28 +0000

    Reset test epoch to 20 for testing.

--- SLDNet_code/test_video_rain.sh
+++ SLDNet_code/test_video_rain.sh
@@ -1,1 +1,1 @@
-python test_video_rain.py -method P401_video_rain_self -epoch 75 -dataset Video_rain -task RainRemoval/original -data_dir data_NTU -model_name derain_self -checkpoint_dir ./checkpoints/P401_video_rain_self/ -list_filename ./lists/video_rain_removal_test.txt
+python test_video_rain.py -method P401_video_rain_self -epoch 20 -dataset Video_rain -task RainRemoval/original -data_dir data_NTU -model_name derain_self -checkpoint_dir ./checkpoints/P401_video_rain_self/ -list_filename ./lists/video_rain_removal_test.txt
