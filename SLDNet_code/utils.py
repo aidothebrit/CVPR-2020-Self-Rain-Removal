9189361e497827f3291e109f5b3f4b32a6f1f169
Adrian Creegan <adrian.creegan2@bd.com>
2023-02-06 14:52:18 +0000

    More messing around with folders.

--- SLDNet_code/utils.py
+++ SLDNet_code/utils.py
@@ -276,7 +276,7 @@ class SubsetSequentialSampler(Sampler):
         self.indices = indices
 
     def __iter__(self):
-        print(self.indices[i*3] for i in range(len(self.indices)))
+        print(self.indices[i*3]) for i in range(len(self.indices))
         return (self.indices[i*3] for i in range(len(self.indices)))
 
     def __len__(self):
